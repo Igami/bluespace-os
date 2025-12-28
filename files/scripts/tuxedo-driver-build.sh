9c476dc8aeba8d0fe124ed3f28cf07a681b73a0c
igami <igami@mailbox.org>
2025-12-28 21:37:34 +0100

    try add tuxedo-drivers

---
+++ files/scripts/tuxedo-driver-build.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+RELEASE="$(rpm -E %fedora)"
+
+### Install packages
+
+#Exec perms for symlink script
+chmod +x /usr/bin/fixtuxedo
+#And autorun
+systemctl enable /etc/systemd/system/fixtuxedo.service
+
+KERNEL="$(rpm -q kernel --queryformat '%{VERSION}')"
+echo "Kernel: ${KERNEL}"
+
+rpm-ostree install "kernel-devel-$KERNEL"
+
+export HOME=/tmp
+
+cd /tmp
+
+rpmdev-setuptree
+
+git clone https://github.com/gladion136/tuxedo-drivers-kmod
+
+cd tuxedo-drivers-kmod/
+./build.sh
+cd ..
+
+# Extract the Version value from the spec file
+export TD_VERSION=$(cat tuxedo-drivers-kmod/tuxedo-drivers-kmod-common.spec | grep -E '^Version:' | awk '{print $2}')
+export KERNEL_VERSION="$(rpm -q kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
+
+echo "Kernel version: ${KERNEL_VERSION}"
+echo "Tuxedo version: ${TD_VERSION}"
+
+rpm-ostree install ~/rpmbuild/RPMS/x86_64/akmod-tuxedo-drivers-$TD_VERSION-1.fc42.x86_64.rpm ~/rpmbuild/RPMS/x86_64/tuxedo-drivers-kmod-$TD_VERSION-1.fc42.x86_64.rpm ~/rpmbuild/RPMS/x86_64/tuxedo-drivers-kmod-common-$TD_VERSION-1.fc42.x86_64.rpm ~/rpmbuild/RPMS/x86_64/kmod-tuxedo-drivers-$TD_VERSION-1.fc42.x86_64.rpm
+
+akmods --force --kernels "${KERNEL_VERSION}" --kmod "tuxedo-drivers-kmod"
+
+#Hacky workaround to make TCC install elsewhere
+mkdir -p /usr/share
+rm /opt
+ln -s /usr/share /opt
+
+rpm-ostree install tuxedo-control-center
+
+cd /
+rm /opt
+ln -s var/opt /opt
+ls -al /
+
+rm /usr/bin/tuxedo-control-center
+ln -s /usr/share/tuxedo-control-center/tuxedo-control-center /usr/bin/tuxedo-control-center
+
+sed -i 's|/opt|/usr/share|g' /etc/systemd/system/tccd.service
+sed -i 's|/opt|/usr/share|g' /usr/share/applications/tuxedo-control-center.desktop
+
+systemctl enable tccd.service
+
+systemctl enable tccd-sleep.service
+
+systemctl enable podman.socket
